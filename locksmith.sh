9dfcdd4a52ea60713c0606357451b47b4f7ae106
stuartminch <stuart@haxtable.com>
2014-07-07 10:28:45 -0700

    Create locksmith.sh

---
+++ locksmith.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+LOG=/var/www/locksmith/locksmith.log
+
+# New log entry
+START_DATE=$(date)
+echo "Script started: $START_DATE" >> $LOG
+
+# Lock creation/verification
+if mkdir /var/www/lock; then
+
+  echo "Locking succeeded" >> $LOG
+else
+  echo "Lock failed - exit" >> $LOG
+  exit 1
+fi
+
+# Rsync job
+rsync -aHyP --bwlimit=2000 --log-file=$LOG --exclude='some_dir' rsync://x.x.x.x/some/remote_dir/. /some/local_dir
+
+# Remove lock
+rm -rf /var/www/lock
+
+
+# Close out log entry
+END_DATE=$(date)
+echo "Script completed: $END_DATE" >> $LOG
